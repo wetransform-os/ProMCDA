c95bb7c72ba222908f772d0b6eb2283566916ad9
Flaminia <flaminia.catalli@gmail.com>
2023-05-03 15:54:15 +0200

    added all normalization functions and unit tests

--- mcda/utility_functions/normalization.py
+++ mcda/utility_functions/normalization.py
@@ -2,6 +2,7 @@ import copy
 import logging
 import numpy as np
 import pandas as pd
+from typing import List, Union
 from sklearn import preprocessing
 
 class Normalization(object):
@@ -10,19 +11,46 @@ class Normalization(object):
 
     This class normalizes the values of each indicator in the input_matrix
     by mean of different normalization functions.
+    Each function rescales the values by considering their initial polarity.
+    If the polarity is negative (i.e. the scale is reversed), then the
+    smallest the value the better.
+
+    Type of normalization functions
+    Ordinal: rank
+    Interval: standardized; min-max
+    Ratio: target
 
     """
 
-    def __init__(self, input_matrix: pd.DataFrame()):
+    def __init__(self, input_matrix: pd.DataFrame(), polarities):
 
         self._input_matrix = copy.deepcopy(input_matrix)
+        self.polarities = polarities
+
+
+    def _cast_polarities(self) -> List:
+        """
+        Identifies indicators with positive
+        or negative polarity (by indexes) and
+        cast them into two separate dfs
+
+        :data: input matrix
+        :returns: list of 2 lists for pos and neg indexes, and 2 dfs
+        """
+
+        ind_plus = [i for i, e in enumerate(self.polarities) if e == "+"]
+        ind_minus = [i for i, e in enumerate(self.polarities) if e == "-"]
+        indicators_plus = self._input_matrix.iloc[:, ind_plus]
+        indicators_minus = self._input_matrix.iloc[:, ind_minus]
+
+        return (ind_plus, ind_minus, indicators_plus, indicators_minus)
 
     @staticmethod
     def reversed_minmax_scaler(data, feature_range):
         """
-        Rescales the given data in a reversed scale
+        Rescales the indicators in a reversed scale
         where the smallest the value the better,
-        using the scaling method min-max.
+        by using the scaling method min-max.
 
         :param: range defines if the feature range is (0,1) or (0.1,1)
         :returns: numpy array
@@ -40,23 +68,21 @@ class Normalization(object):
 
         return scaled_data
 
-    def minmax(self, polarities, feature_range) -> pd.DataFrame():
+    def minmax(self, feature_range) -> pd.DataFrame():
         """
-        Normalizes the given data by using the scaling method min-max.
-        It rescales the values by considering also their initial polarity.
-        If the polarity is negative (i.e. the scale is reversed), then the
-        smallest the value the better.
+        Normalizes the indicators by using the scaling method min-max.
         Different feature ranges are possible.
 
         :returns: pd.DataFrame()
         """
+
         original_shape = self._input_matrix.shape
 
-        # identify indicators with positive or negative polarity and cast into different dfs
-        ind_plus = [i for i, e in enumerate(polarities) if e == "+"]
-        ind_minus = [i for i, e in enumerate(polarities) if e == "-"]
-        indicators_plus = self._input_matrix.iloc[:, ind_plus]
-        indicators_minus = self._input_matrix.iloc[:, ind_minus]
+        pol = Normalization._cast_polarities(self)
+        ind_plus = pol[0]
+        ind_minus = pol[1]
+        indicators_plus = pol[2]
+        indicators_minus = pol[3]
 
         # for + polarity
         x = indicators_plus.to_numpy() # returns a numpy array
@@ -72,22 +98,87 @@ class Normalization(object):
         # merge back scaled values for positive and negative polarities
         indicators_scaled_minmax = pd.DataFrame(index=range(original_shape[0]), columns=range(original_shape[1]))
         for i,index_p in enumerate(ind_plus): indicators_scaled_minmax.iloc[:, index_p] = indicators_scaled_minmax_plus.iloc[:, i]
-        for j, index_n in enumerate(ind_minus): indicators_scaled_minmax.iloc[:, index_n]=indicators_scaled_minmax_minus.loc[:,j]
+        for j, index_n in enumerate(ind_minus): indicators_scaled_minmax.iloc[:, index_n]=indicators_scaled_minmax_minus.iloc[:,j]
 
         return indicators_scaled_minmax
 
 
-    def target(self, polarities) -> pd.DataFrame():
+    def target(self, feature_range) -> pd.DataFrame():
         """
-        Normalize indicators using the scaling method target.
+        Normalizes indicators using the scaling method target.
         :return: pd.DataFrame()
         """
 
-        x = self._input_matrix
+        original_shape = self._input_matrix.shape
 
-        if x.max().all() > 0:
-            indicators_scaled_target = x/x.max()
+        pol = Normalization._cast_polarities(self)
+        ind_plus = pol[0]
+        ind_minus = pol[1]
+        indicators_plus = pol[2]
+        indicators_minus = pol[3]
+
+        if self._input_matrix.values.all() > 0:
+            if (feature_range == (0,1)):
+                indicators_scaled_target_plus = indicators_plus/indicators_plus.max(axis=0) # for + polarity
+                indicators_scaled_target_minus = 1-indicators_minus/indicators_minus.max(axis=0) # for - polarity
+            else:
+                indicators_scaled_target_plus = indicators_plus/indicators_plus.max(axis=0)*(1-0.1) + 0.1 # for + polarity
+                indicators_scaled_target_minus = (1-indicators_minus/indicators_minus.max(axis=0))*(1-0.1) + 0.1 # for - polarity
         else:
-            raise ValueError('Indicators in the input matrix should have values larger than 0.')
+            raise ValueError('Indicators in the input matrix should have all positive values.')
+
+        # merge back scaled values for positive and negative polarities
+        indicators_scaled_target = pd.DataFrame(index=range(original_shape[0]), columns=range(original_shape[1]))
+        for i,index_p in enumerate(ind_plus): indicators_scaled_target.iloc[:, index_p] = indicators_scaled_target_plus.iloc[:, i]
+        for j, index_n in enumerate(ind_minus): indicators_scaled_target.iloc[:, index_n] = indicators_scaled_target_minus.iloc[:,j]
 
         return indicators_scaled_target
+
+
+    def standardized(self) -> pd.DataFrame():
+        """
+        Normalizes indicators using the scaling method standardized (i.e. Z-score).
+        :return: pd.DataFrame()
+        """
+
+        original_shape = self._input_matrix.shape
+
+        pol = Normalization._cast_polarities(self)
+        ind_plus = pol[0]
+        ind_minus = pol[1]
+        indicators_plus = pol[2]
+        indicators_minus = pol[3]
+
+        indicators_scaled_stand_plus = (indicators_plus - indicators_plus.mean(axis=0))/indicators_plus.std(axis=0) # for + polarity
+        indicators_scaled_stand_minus = (indicators_minus.mean(axis=0) - indicators_minus)/indicators_minus.std(axis=0) # for - polarity
+
+        # merge back scaled values for positive and negative polarities
+        indicators_scaled_standardized = pd.DataFrame(index=range(original_shape[0]), columns=range(original_shape[1]))
+        for i,index_p in enumerate(ind_plus): indicators_scaled_standardized.iloc[:, index_p] = indicators_scaled_stand_plus.iloc[:, i]
+        for j, index_n in enumerate(ind_minus): indicators_scaled_standardized.iloc[:, index_n] = indicators_scaled_stand_minus.iloc[:,j]
+
+        return indicators_scaled_standardized
+
+
+    def rank(self) -> pd.DataFrame():
+        """
+        Normalizes indicators using the scaling method rank.
+        :return: pd.DataFrame()
+        """
+        original_shape = self._input_matrix.shape
+
+        pol = Normalization._cast_polarities(self)
+        ind_plus = pol[0]
+        ind_minus = pol[1]
+        indicators_plus = pol[2]
+        indicators_minus = pol[3]
+
+        indicators_scaled_rank_plus = indicators_plus.rank(axis=0) # for + polarity
+        indicators_scaled_rank_minus = (-1*indicators_minus).rank(axis=0) # for - polarity
+
+        # merge back scaled values for positive and negative polarities
+        indicators_scaled_rank = pd.DataFrame(index=range(original_shape[0]), columns=range(original_shape[1]))
+        for i,index_p in enumerate(ind_plus): indicators_scaled_rank.iloc[:, index_p] = indicators_scaled_rank_plus.iloc[:, i]
+        for j, index_n in enumerate(ind_minus): indicators_scaled_rank.iloc[:, index_n] = indicators_scaled_rank_minus.iloc[:,j]
+
+        return indicators_scaled_rank
--- tests/unit_tests/test_normalization.py
+++ tests/unit_tests/test_normalization.py
@@ -8,30 +8,93 @@ from mcda.utility_functions.normalization import Normalization
 
 class TestNormalization(unittest.TestCase):
 
-    def test_minmax(self):
-        # Given
-        polarities = ("-","-","+","+","+","+") # same as in MCDTool/configuration_without_uncertainty.json
+    @staticmethod
+    def get_input_matrix():
         input_matrix = read_matrix("tests/resources/input_matrix_without_uncert.csv")
         input_matrix_no_alternatives = input_matrix.drop(input_matrix.columns[0],axis=1) # drop first column with alternatives
                                                                                          # in the code this is happening in mcda_run
-        # When
-        expected_res_01 = read_matrix('tests/resources/normalization/res_minmax_pol_01.csv')
-        expected_res_no0 = read_matrix('tests/resources/normalization/res_minmax_no0.csv')
+        return input_matrix_no_alternatives
 
-        norm = Normalization(input_matrix_no_alternatives)
-        res_01 = norm.minmax(polarities, feature_range=(0, 1))
+    @staticmethod
+    def get_input_polarities():
+        polarities = ("-","-","+","+","+","+") # same as in MCDTool/configuration_without_uncertainty.json
+
+        return polarities
+
+    def test_minmax(self):
+        # Given
+        polarities = TestNormalization.get_input_polarities()
+        input_matrix_no_alternatives = TestNormalization.get_input_matrix()
+
+        # When
+        expected_res_01 = read_matrix('tests/resources/normalization/res_minmax_01.csv')
+        expected_res_no0 = read_matrix('tests/resources/normalization/res_minmax_no0.csv')
+        norm = Normalization(input_matrix_no_alternatives, polarities)
+        res_01 = norm.minmax(feature_range=(0, 1))
         res_01.columns = res_01.columns.astype('str') # to match the type of columns in the two dfs
-        res_no0 = norm.minmax(polarities, feature_range=(0.1, 1))
+        res_no0 = norm.minmax(feature_range=(0.1, 1))
         res_no0.columns = res_no0.columns.astype('str') # to match the type of columns in the two dfs
 
-
         # Then
-        print(res_01)
-        print(expected_res_01)
         assert isinstance(res_01, pd.DataFrame)
         assert isinstance(res_no0, pd.DataFrame)
         assert_frame_equal(res_01, expected_res_01, check_like=True)
         assert_frame_equal(res_no0, expected_res_no0, check_like=True)
+
+
+    def test_target(self):
+        # Given
+        polarities = TestNormalization.get_input_polarities()
+        input_matrix_no_alternatives = TestNormalization.get_input_matrix()
+
+        # When
+        expected_res_01 = read_matrix('tests/resources/normalization/res_target_01.csv')
+        expected_res_no0 = read_matrix('tests/resources/normalization/res_target_no0.csv')
+        norm = Normalization(input_matrix_no_alternatives,polarities)
+        res_01 = norm.target(feature_range=(0, 1))
+        res_01.columns = res_01.columns.astype('str') # to match the type of columns in the two dfs
+        res_no0 = norm.target(feature_range=(0.1, 1))
+        res_no0.columns = res_no0.columns.astype('str') # to match the type of columns in the two dfs
+
+        # Then
+        assert isinstance(res_01, pd.DataFrame)
+        assert isinstance(res_no0, pd.DataFrame)
+        assert_frame_equal(res_01, expected_res_01, check_like=True)
+        assert_frame_equal(res_no0, expected_res_no0, check_like=True)
+
+
+    def test_standardized(self):
+        # Given
+        polarities = TestNormalization.get_input_polarities()
+        input_matrix_no_alternatives = TestNormalization.get_input_matrix()
+
+        # When
+        expected_res = read_matrix('tests/resources/normalization/res_standardized.csv')
+        norm = Normalization(input_matrix_no_alternatives,polarities)
+        res = norm.standardized()
+        res.columns = res.columns.astype('str') # to match the type of columns in the two dfs
+
+        # Then
+        assert isinstance(res, pd.DataFrame)
+        assert_frame_equal(res, expected_res, check_like=True)
+
+
+
+    def test_rank(self):
+        # Given
+        polarities = TestNormalization.get_input_polarities()
+        input_matrix_no_alternatives = TestNormalization.get_input_matrix()
+
+        # When
+        expected_res = read_matrix('tests/resources/normalization/res_rank.csv')
+        norm = Normalization(input_matrix_no_alternatives,polarities)
+        res = norm.rank()
+        res.columns = res.columns.astype('str') # to match the type of columns in the two dfs
+
+        # Then
+        assert isinstance(res, pd.DataFrame)
+        assert_frame_equal(res, expected_res, check_like=True)
+
 
         if __name__ == '__main__':
             unittest.main()
