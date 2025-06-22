39e8067ba0be116ba0fbce6ab262bbc55ee6ebe5
Flaminia <flaminia.catalli@gmail.com>
2025-06-22 20:05:42 +0200

    remove hard paths from demo

--- demo_in_notebook/test_import.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-# Set the package path to the parent directory
-package_path = '/Users/flaminia/Documents/work/ProMCDA'
-sys.path.append(package_path)
-
-# Now import ProMCDA from models
-from promcda.models.ProMCDA import ProMCDA
-print("Import successful!")
